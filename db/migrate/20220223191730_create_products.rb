5088ae47d6787c254b287043a5bbff34510778fb
Arthur Rezende <arthur.rezgod@gmail.com>
2022-02-23 17:57:45 -0300

    Criando app OneBitFood

---
+++ db/migrate/20220223191730_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.text :namedescription
+      t.float :price
+      t.references :product_category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
